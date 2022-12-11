fcf129b4c83a3a8c0e65a1308a21f593e9a62d17
Iakub94 <119245021+Iakub94@users.noreply.github.com>
2022-12-11 22:25:12 +0300

    Order #2

--- HomeWorkSeminar#4/Program.cs
+++ HomeWorkSeminar#4/Program.cs
@@ -11,12 +11,25 @@ Console.Clear();
 
 // Задача 2. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
-// Console.Clear();
+Console.Clear();
 
-// int SumDigits (int number)
-// {
-//     int number = 
-// }
+int SumDigits (int number)
+{
+    int sum = 0;
+    while (number <= 0)
+    {
+        sum += number;
+        number--;
+    }
+    return sum;
+}
+
+Console.WriteLine("Input number: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int result = SumDigits(number);
+
+Console.WriteLine($"Sum of numbers is {result}");
 
 
 
